2d2caec54cc27a35321b0bbbe5189884c6df0bb7
nemo <nemo.xd5@gmail.com>
2022-09-09 18:16:39 +0900

    twindやめる

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,20 +1,21 @@
 /** @jsx h */
 import { h } from "preact";
-import { tw } from "@twind";
-import Counter from "../islands/Counter.tsx";
+import { InputBox } from "~/components/InputBox.tsx";
+import Counter from "~/islands/Counter.tsx";
 
 export default function Home() {
   return (
-    <div class={tw`p-4 mx-auto max-w-screen-md`}>
+    <div>
       <img
         src="/logo.svg"
         height="100px"
         alt="the fresh logo: a sliced lemon dripping with juice"
       />
-      <p class={tw`my-6`}>
-        Welcome to `fresh`. Try update this message in the ./routes/index.tsx
+      <p>
+        Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
         file, and refresh.
       </p>
+      <InputBox />
       <Counter start={3} />
     </div>
   );
